f2ab457432e40821deba731da04b9158c5415dcf
Pablo Ruiz <pablo_ruiza@hotmail.com>
2024-11-25 17:18:55 -0500

    create appBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:twitter/screen/home_screen.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 
 void main() => runApp(const MyApp());
@@ -26,10 +27,54 @@ class Twitter extends StatelessWidget {
       child: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.black,
-          title: const Icon(
-            Icons.home,
-            color: Colors.white
-          )
+          toolbarHeight: 80.0,
+          flexibleSpace: Padding(
+            padding: const EdgeInsets.only(top: 50.0),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 20.0),
+                  child: Stack(
+                    children: [
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Container(
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.grey,
+                          ),
+                          padding: const EdgeInsets.all(8.0),
+                          child: const Text(
+                            'P',
+                            style: TextStyle(color: Colors.white, fontSize: 16.0),
+                          ),
+                        ),
+                      ),
+                      const Align(
+                        alignment: Alignment.center, 
+                        child: Icon(
+                          FontAwesomeIcons.xTwitter,
+                          color: Colors.white,
+                          size: 30.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 55.0, vertical: 10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('For you', style: TextStyle(color: Colors.white, fontSize: 16.0, fontWeight: FontWeight.bold,),),
+                      Text('Following', style: TextStyle(color: Colors.white, fontSize: 16.0, fontWeight: FontWeight.bold,),),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          
         ),
         body: const TabBarView(children: [HomeScreen()])
       ),

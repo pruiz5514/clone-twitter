9f4290c5daf9bb2d78261c777770052524c32887
Pablo Ruiz <pablo_ruiza@hotmail.com>
2024-11-26 10:27:42 -0500

    he widget is parameterized

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -9,14 +9,53 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: <Widget>[
-          tweets(context),
+          tweets(
+            context,
+            profileImage: 'https://pbs.twimg.com/profile_images/1810693638973779968/3PB9X9HL_400x400.jpg',
+            name:'Atlético Nacional',
+            userName: '@nacionaloficial',
+            verified: true,
+            date: '27/07/16',
+            content: 'Somos el único equipo del país con dos títulos de Copa Libertadores. ¡Conquistamos América en 1989 y 2016!',
+            tweetImage: 'https://pbs.twimg.com/media/Coa0yKMWcAAvKHK?format=jpg&name=small',
+            comments: '92',
+            retweets: '2.8K',
+            likes: '3K',
+            views: '111K',
+          ),
+          tweets(
+            context,
+            profileImage: 'https://pbs.twimg.com/profile_images/1810693638973779968/3PB9X9HL_400x400.jpg',
+            name:'Atlético Nacional',
+            userName: '@nacionaloficial',
+            verified: true,
+            date: '27/07/16',
+            content: 'NAL vs IDV |1-0| Termina el encuentro por la final de la libertadores. ¡SOMOS CAMPEONES DE AMERICA!' ,
+            comments: '292',
+            retweets: '9.8K',
+            likes: '23K',
+            views: '111K',
+          ),
           
         ],
       ),
     );
   }
 
-  Widget tweets(BuildContext context) {
+  Widget tweets(
+    BuildContext context,{
+      String? profileImage,
+      required String name,
+      required String userName,
+      required bool verified,
+      required String date,
+      required String content,
+      String? tweetImage,
+      required String comments,
+      required String retweets,
+      required String likes,
+      required String views,
+    }) {
     return Container(
       color: Colors.black,
       child: Column(
@@ -32,6 +71,7 @@ class HomeScreen extends StatelessWidget {
             child: Row(
               crossAxisAlignment: CrossAxisAlignment.start, 
               children: [
+                profileImage != null ?
                 Container(
                   width: 40.0,
                   height: 40.0,
@@ -44,7 +84,17 @@ class HomeScreen extends StatelessWidget {
                       fit: BoxFit.cover,
                     ),
                   ),
+                ) :
+                Container(
+                  width: 40.0,
+                  height: 40.0,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.grey,
+                  ),
+                  child: const Icon(Icons.person, color: Colors.white),
                 ),
+
                 const SizedBox(width: 10.0),
                 Expanded(
                   child: Column(
@@ -52,76 +102,79 @@ class HomeScreen extends StatelessWidget {
                     children: [
                       Row(
                         children: [
-                          const Text(
-                            'Atlético Nacional',
-                            style: TextStyle(
+                          Text(
+                            name,
+                            style: const TextStyle(
                               color: Colors.white,
                               fontWeight: FontWeight.bold,
                             ),
                           ),
                           const SizedBox(width: 4.0),
-                          Container(
-                            width: 16.5,
-                            height: 16.5,
-                            decoration: const BoxDecoration(
-                              image: DecorationImage(
-                                image: NetworkImage(
-                                  'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Twitter_Verified_Badge.svg/640px-Twitter_Verified_Badge.svg.png',
+                          
+                          if(verified)
+                            Container(
+                              width: 16.5,
+                              height: 16.5,
+                              decoration: const BoxDecoration(
+                                image: DecorationImage(
+                                  image: NetworkImage(
+                                    'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Twitter_Verified_Badge.svg/640px-Twitter_Verified_Badge.svg.png',
+                                  ),
+                                  fit: BoxFit.cover,
                                 ),
-                                fit: BoxFit.cover,
                               ),
                             ),
+                            const SizedBox(width: 4.0),
+                          Text(
+                            userName,
+                            style: const TextStyle(color: Color(0xFF71767B)),
                           ),
                           const SizedBox(width: 4.0),
-                          const Text(
-                            '@nacionaloficial',
-                            style: TextStyle(color: Color(0xFF71767B)),
-                          ),
-                          const SizedBox(width: 4.0),
-                          const Text(
-                            '27/07/16',
-                            style: TextStyle(color: Color(0xFF71767B)),
+                          Text(
+                            date,
+                            style: const TextStyle(color: Color(0xFF71767B)),
                           ),
                         ],
                       ),
                       const SizedBox(height: 4.0),
-                      const Text(
-                        'Somos el único equipo del país con dos títulos de Copa Libertadores. ¡Conquistamos América en 1989 y 2016!',
-                        style: TextStyle(color: Colors.white),
+                      Text(
+                        content,
+                        style: const TextStyle(color: Colors.white),
                         softWrap: true, 
                         overflow: TextOverflow.visible, 
                       ),
                       const SizedBox(height: 10.0),
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(12.0),
-                        child: Image.network('https://pbs.twimg.com/media/Coa0yKMWcAAvKHK?format=jpg&name=small',
-                          width: double.infinity,
-                          height: 220.0,
-                          fit: BoxFit.cover,
+                      if(tweetImage != null)
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(12.0),
+                          child: Image.network('https://pbs.twimg.com/media/Coa0yKMWcAAvKHK?format=jpg&name=small',
+                            width: double.infinity,
+                            height: 220.0,
+                            fit: BoxFit.cover,
+                          ),
                         ),
-                      ),
                       const SizedBox(height: 10.0),
-                      const Row(
+                      Row(
                         children: [
-                          Icon(FontAwesomeIcons.comment,color:  Color(0xFF71767B), size: 15.0,),
-                          SizedBox(width: 5.0,),
-                          Text('92', style: TextStyle(color: Color(0xFF71767B), fontSize: 13.0),),
-                          SizedBox(width: 22.0,),
-                          Icon(FontAwesomeIcons.retweet,color:  Color(0xFF71767B), size: 15.0,),
-                          SizedBox(width: 5.0,),
-                          Text('2.8K', style: TextStyle(color: Color(0xFF71767B), fontSize: 13.0),),
-                          SizedBox(width: 22.0,),
-                          Icon(FontAwesomeIcons.solidHeart, color:  Colors.red, size: 15.0,),
-                          SizedBox(width: 5.0,),
-                          Text('3K', style: TextStyle(color: Color(0xFF71767B), fontSize: 13.0),),
-                          SizedBox(width: 22.0,),
-                          Icon(FontAwesomeIcons.chartSimple,color:  Color(0xFF71767B), size: 15.0,),
-                          SizedBox(width: 5.0,),
-                          Text('111K', style: TextStyle(color: Color(0xFF71767B), fontSize: 13.0),),
-                          SizedBox(width: 22.0,),
-                          Icon(FontAwesomeIcons.bookmark,color:  Color(0xFF71767B), size: 15.0,),
-                          SizedBox(width: 22.0,),
-                          Icon(FontAwesomeIcons.squareShareNodes,color:  Color(0xFF71767B), size: 15.0,),
+                          const Icon(FontAwesomeIcons.comment,color:  Color(0xFF71767B), size: 15.0,),
+                          const SizedBox(width: 5.0,),
+                          Text(comments, style: const TextStyle(color: Color(0xFF71767B), fontSize: 13.0),),
+                          const SizedBox(width: 20.0,),
+                          const Icon(FontAwesomeIcons.retweet,color:  Color(0xFF71767B), size: 15.0,),
+                          const SizedBox(width: 5.0,),
+                          Text(retweets, style: const TextStyle(color: Color(0xFF71767B), fontSize: 13.0),),
+                          const SizedBox(width: 20.0,),
+                          const Icon(FontAwesomeIcons.solidHeart, color:  Colors.red, size: 15.0,),
+                          const SizedBox(width: 5.0,),
+                          Text(likes, style: const TextStyle(color: Color(0xFF71767B), fontSize: 13.0),),
+                          const SizedBox(width: 20.0,),
+                          const Icon(FontAwesomeIcons.chartSimple,color:  Color(0xFF71767B), size: 15.0,),
+                          const SizedBox(width: 5.0,),
+                          Text(views, style: const TextStyle(color: Color(0xFF71767B), fontSize: 13.0),),
+                          const SizedBox(width: 20.0,),
+                          const Icon(FontAwesomeIcons.bookmark,color:  Color(0xFF71767B), size: 15.0,),
+                          const SizedBox(width: 20.0,),
+                          const Icon(FontAwesomeIcons.squareShareNodes,color:  Color(0xFF71767B), size: 15.0,),
                           
                         ],
                       )
